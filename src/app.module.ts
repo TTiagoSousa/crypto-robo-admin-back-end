938a850d135a50df9938681bdb20e24740d4daa9
ttiagosousa <ttiagopintosousa97@gmail.com>
2025-03-12 17:12:51 +0000

    Employee service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthEmployeeModule } from './auth-employee/auth-employee.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [AuthEmployeeModule],
+  imports: [AuthEmployeeModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
